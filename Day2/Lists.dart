95882ed2a4d4ea4c2b8b24df210f830491c61a2e
Baris <barsgzei@outlook.com>
2024-04-18 19:10:57 +0100

    listssss finished

--- Day2/Lists.dart
+++ Day2/Lists.dart
@@ -11,19 +11,36 @@ void main(List<String> args) {
 
   int number = 21;
   List<int> numbers= List.filled(5, number,growable: true);
-  print(numbers);
-  printer(numbers);
+  //print(numbers);
+  //printer(numbers);
 
   // DYNAMICCCS vaowwww you dont have to specify list type it will be dynamic automatically
-  List<dynamic> dynamiclist = List.filled(7, "sena");
-  print (dynamiclist);
-  dynamiclist[1] ="Baris'a";
+  List<dynamic> dynamiclist = List.filled(7, "Mikail");
+  //print (dynamiclist);
+  dynamiclist[1] ="and";
   dynamiclist[2] = 7;         // integer
-  dynamiclist[3] ="gundur";
+  dynamiclist[3] ="brothers";
   dynamiclist[4] = 21.5;      // double
-  dynamiclist[5] ="kerer";
-  dynamiclist[6] = "kustu";
-  print(dynamiclist);  
+  dynamiclist[5] ="battle";
+  dynamiclist[6] = "lie";
+  //print(dynamiclist);  
+
+  List<double> Numerikos = List.filled(5, 21.0,growable: true);
+  Numerikos[3]=13.5;
+  Numerikos[2]=3.5;
+  Numerikos[4]=43.5;
+  Numerikos[0]=43.2;
+  print(Numerikos.first);
+  print(Numerikos.indexOf(13.5));
+  print(Numerikos.last);
+  print(Numerikos.firstOrNull);
+  print(Numerikos.firstWhere((element) => element<14));
+  Numerikos.shuffle();
+  print(Numerikos);
+  Numerikos.sort();
+  print(Numerikos);
+
+
 }
 
 printer(List<int> nums){
