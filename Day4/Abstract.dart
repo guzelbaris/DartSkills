07085be5dea4941f66f4b0237b38be8c46bd603c
Baris <barsgzei@outlook.com>
2024-04-21 23:55:39 +0100

    intro to abstract

---
+++ Day4/Abstract.dart
@@ -0,0 +1,11 @@
+/*
+  To define a class that doesn't require a full, concrete implementation of its entire interface, use the abstract  modifier.
+
+  Abstract classes cannot be constructed from any library, whether its own or an outside library. Abstract classes  often have abstract methods.
+ */
+void main(List<String> args) {
+  
+}
+
+
+
