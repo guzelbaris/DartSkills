673aeccf27d492581c04e5f36085e988a632a271
Baris <barsgzei@outlook.com>
2024-04-22 00:09:49 +0100

    subclasses created and functions are override

--- Day4/Abstract.dart
+++ Day4/Abstract.dart
@@ -3,7 +3,7 @@
 
   Abstract classes cannot be constructed from any library, whether its own or an outside library. Abstract classes  often have abstract methods.
  */
-import 'dart:ffi';
+
 
 void main(List<String> args) {
   
@@ -11,6 +11,38 @@ void main(List<String> args) {
 
 // Abstract class created need one abstract method
 abstract class Shapes {
-  Double area();
+  double area(); // no return type defined
+  double perimeter();
+}
+
+class Square extends Shapes {
+  int vertice=0;
+  Square(this.vertice);
+  @override
+  double area() {
+    return 2*this.vertice*this.vertice.toDouble();
+  }
+
+  @override
+  double perimeter() {
+    return 4*this.vertice.toDouble();
+  }
+  
+}
+
+class Rectangle extends Shapes {
+  int shortside=0;
+  int longside=0;
+  
+  @override
+  double area() {
+    return 2*this.shortside*this.longside.toDouble();
+  }
+  
+  @override
+  double perimeter() {
+    return 2*(this.shortside+this.longside).toDouble();
+  }
+  
 }
 
